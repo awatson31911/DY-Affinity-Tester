46cbdfd881d9d67d35eb6175f9ce3137d127772a
Anthony <awatson31911@gmail.com>
2019-07-23 18:19:16 -0400

    updates readme

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Affinity Tester
+ A Chrome Extension to track a user's affinity to different content and change UI based on it. This Extension focuses on the New Arrivals Page of the Urban Outfitters website.
 
-### A Chrome Extension to track a user's affinity to different content and display content differently based on it. This Extension focuses on the New Arrivals Page on the Urban Outfitters website.
 
-#### INSERT INFO ON HOW TO GET THE CHROME EXTENSION HERE
+## Technologies/Services Used
+- Javascript
+- Google Chrome Extension API
+- HTML
+- CSS
+
+## Summary
+Information is taken from the web page and sent back to the extension to display a user's affinity for specific categories: women's fashion, men's fashioni, beauty, home and lifestyle. The extension also rearranges the New-Arrivals page to place higher ranking categories closer to the top of the page.
+
+The extension uses chrome messaging to communicate from the web page to the extension and vice versa. Namely the functions:
+
+```
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+}
+```
+
+and 
+
+```
+chrome.runtime.sendMessage({ })
+```
+
+Information is transferred from the UO webpage, via content.js, to the background page eventPage.js then to the extension's popup.js.
--- content_NewArrivalsPage.js
+++ content_NewArrivalsPage.js
@@ -1,23 +1,25 @@
-const affinity = JSON.parse(localStorage.getItem('CSE_Challenge'));
+if (localStorage.getItem('CSE_Challenge')) {
+  const affinity = JSON.parse(localStorage.getItem('CSE_Challenge'));
 
-const sanitizeHeader = (header) => {
-  return header.replace('\'', '').toLowerCase();
-};
+  const sanitizeHeader = (header) => {
+    return header.replace('\'', '').toLowerCase();
+  };
 
-const nodes = Array.from(document.querySelectorAll('div[data-qa-module-type=categoryProductTray]'))
-  .map((node) => {
-    const category = node.querySelector('h2.c-product-tray__h2').innerText;
-    const cleanCategory = sanitizeHeader(category);
-    const score = affinity[cleanCategory];
-    node = node.cloneNode(true);
-    return { node, score: score };
-  })
-  .sort((a, b) => b.score - a.score);
+  const nodes = Array.from(document.querySelectorAll('div[data-qa-module-type=categoryProductTray]'))
+    .map((node) => {
+      const category = node.querySelector('h2.c-product-tray__h2').innerText;
+      const cleanCategory = sanitizeHeader(category);
+      const score = affinity[cleanCategory];
+      node = node.cloneNode(true);
+      return { node, score: score };
+    })
+    .sort((a, b) => b.score - a.score);
 
-let index = 0;
-while (index <= 4) {
-  const replacementNode = nodes[index].node;
+  let index = 0;
+  while (index <= 4) {
+    const replacementNode = nodes[index].node;
 
-  document.querySelectorAll('div[data-qa-module-type=categoryProductTray]')[index].replaceWith(replacementNode);
-  index++;
+    document.querySelectorAll('div[data-qa-module-type=categoryProductTray]')[index].replaceWith(replacementNode);
+    index++;
+  }
 }
